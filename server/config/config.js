8c6bd7f5945d5764c34d8e5955cdcf79b833b548
David <davidchilatra@gmail.com>
2020-08-07 19:12:53 -0500

    Se inicia el proyecto de rest server

---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+/*Configuración puerto */
+process.env.PORT = process.env.PORT || 3000
+/* */
+
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("./config/config");
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/usuario", (req,res) => {
+    res.json("get usuario");
+})
+
+app.post("/usuario", (req,res) => {
+    let body = req.body;
+    res.json({
+        persona:body
+    });
+})
+
+app.put("/usuario/:id", (req,res) => {
+    let id=req.params.id;
+
+    res.json({
+        id
+    });
+    
+})
+
+app.delete("/usuario", (req,res) => {
+    res.json("delete usuario");
+})
+
+
+app.listen(process.env.PORT,() => {
+    console.log("escuchando p 3000")
+})
